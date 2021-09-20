e7eb869956bc4cd134661d418e2da7484b24c84f
tramnewin <trmnguyen2608@gmail.com>
2021-09-20 00:05:46 -0600

    final result with pylint tested

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
+"""
+Name: Tram Nguyen
+CSCI 3415 001
+Assignment 1: Python
+The program read in a grammar from an input file. T
+he grammar file contains the rules in Backus-Naur form
+Each line from the file contains zero or more rules.
+If the line has zero rule, it is considered as a blank line
+and is ignored. All elements of the rule are separated by whitespaces to parse easily.
+Once the program finishes reading the rules of the grammar,
+the program prints out the parsed grammar and then
+read in the user’s sentence and print out the leftmost derivation (if existed) on the terminal.
+"""
 import os
 import re
 import sys
 
 
-def isnonterminal(x):
-    return isinstance(x, str) and re.match(r'^<.*>$', x) is not None
+# isnonterminal function returns true, if the parameter is a nonterminal
+
+def isnonterminal(nonterm):
+    """return boolean datatype"""
+    return isinstance(nonterm, str) and re.match(r'^<.*>$', nonterm) is not None
 
 
-def isterminal(x):
-    return isinstance(x, str) and not isnonterminal(x)
+# isterminal function returns ture if the parameter is a terminal.
+def isterminal(term):
+    """return boolean datatype"""
+    return isinstance(term, str) and not isnonterminal(term)
 
 
+# read_grammar reads the input file, parse the input into rules in which are stored into a list.
 def read_grammar(filepath):
+    """reads in the input text file, parse the input into rules
+    and stores list of rules in grammar"""
     grammar = []
     current_lhs = None
 
@@ -34,8 +55,8 @@ def read_grammar(filepath):
         rules.append(make_rule(current_lhs, rhs))
         return rules
 
-    with open(filepath) as fp:
-        for line in fp:
+    with open(filepath, 'r', encoding='utf-8') as file_path:
+        for line in file_path:
             lexemes = line.split()
             if not lexemes:
                 pass
@@ -53,16 +74,19 @@ def read_grammar(filepath):
 
 
 def print_grammar(grammar):
+    """print out the grammar rules generated from reading the input file"""
     for rule in grammar:
         print(f'{rule[0]} -> {" ".join(rule[1])}')
 
 
-def applicable_rules(grammar, nonterminal): #returns a list of grammar rules for nonterminal
+def applicable_rules(grammar, nonterminal):
+    """returns a list of grammar rules for nonterminal"""
     return list(filter(lambda rule: rule[0] == nonterminal, grammar))
 
 
-def match_form(form, sentence):  # return int of the length of prefix ['begin', 'A', '='] =len(3)
-    for i, lex in enumerate(form):  # i is index and lex is the value within the element of that index, enumerate makes that happen
+def match_form(form, sentence):
+    """return the length of prefix to track how many terminals has been discovered"""
+    for i, lex in enumerate(form):
         if i == len(sentence):
             return -1
         if isnonterminal(lex):
@@ -73,38 +97,55 @@ def match_form(form, sentence):  # return int of the length of prefix ['begin', 
 
 
 def subst(rule, form, match):
+    """return a new form that has the matching form
+    with the list of already found sentential form"""
     return form[:match] + rule[1] + form[match + 1:]
 
-A_fo=[]
-def leftmost_derivation(grammar, sentence, form, finalform=[]):  # my last parameter is incorrect (might be depth
-    # code here
+
+def leftmost_derivation(grammar, sentence, form):  # my last parameter is incorrect (might be depth
+    """find the derivation of the sentence by using this recursive function.
+    The function first find the prefix of the form.
+    if the functions matches the whole length of the sentence,
+    the derivation is finished and return the list of forms
+    if the prefix is -1, none is returned indicating no prefix found.
+    the it goes through a loop going over the list of rules in
+    the grammar. the new form is created by calling subst.
+    if the length of this form is less than or equal to
+    the length of the sentence, this functions is recursively called
+    and store the return result in derivation. If derivation is not
+    empty, we append it into the new created form"""
     match = match_form(form, sentence)
     if match == -1:
         return None
     if match == len(sentence):
-        return finalform
+        return []
     for rule in applicable_rules(grammar, form[match]):
-        if rule not in form and rule[1][0] not in form:
-            form += subst(rule, form, match)
-            finalform += form
-            if len(finalform) > 3:
-                if finalform[max(index for index, item in enumerate(finalform) if item == 'begin'):] == sentence and finalform[max(index for index, item in enumerate(finalform) if item == 'begin'):] != finalform[:3]:
-
-                    print(match, "  ", len(finalform), "   ", finalform)
-                    return finalform
-            leftmost_derivation(grammar, sentence, form[max(index for index, item in enumerate(form) if item == 'begin'):])
+        new_form = subst(rule, form, match)
+        if len(new_form) <= len(sentence):
+            derivation = leftmost_derivation(grammar, sentence, new_form)
+            if derivation is not None:
+                return [new_form] + derivation
     return None
 
 
-
-
-def print_derivation(derivation):
-    for things in derivation:
-        print(f'{things[0]} -> {" ".join(things[1])}')
+def print_derivation(grammar, derivation):
+    """This function prints out the derivation found
+    from the recursive function. The first line should always be
+    <program> -> begin <stmt_list> end for every input sentence.
+    Then, the function goes through the for loop to print out the
+    derivation in a neat format."""
+    print(f'1: {grammar[0][0]} -> {" ".join(grammar[0][1])}')
+    for i, things in enumerate(derivation):
+        print(f'{i + 2}:           -> {things[0]} {" ".join(things[1:])}')
 
 
 def main():
-    #filepath = sys.argv[1]
+    """main program that would calls the above functions in order to
+     reads the grammar file, parse it into list of grammar rules,
+     reads in the user's sentence, split it into a list, then
+     calls the leftmost_derivation function to find setential forms
+     and print out the result onto the terminal"""
+    # filepath = sys.argv[1]
     filepath = input("Name of the file: ")
     if not os.path.isfile(filepath):
         raise Exception(f'File path {filepath} does not exist.')
@@ -123,15 +164,14 @@ def main():
             sys.exit()
         sentence = sentence_string.split()
         print(sentence)
-        #print(sentence)  # added on to check for correct input
+        # print(sentence)  # added on to check for correct input
         start = grammar[0][1]
 
         derivation = leftmost_derivation(grammar, sentence, start)
         print(derivation)
         print(' '.join(sentence))
         print('Derivation:')
-        print_derivation(derivation)
-        print('\nDone')
+        print_derivation(grammar, derivation)
 
 
 if __name__ == '__main__':
